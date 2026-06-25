cd1a4c43b4678d1e92094b8145fdc50355b8cde9
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2026-06-25 16:01:18 +0500

    chore: #504: add comments to tests

--- originjs/cypress/e2e/layout-smoke.cy.ts
+++ originjs/cypress/e2e/layout-smoke.cy.ts
@@ -6,6 +6,8 @@ describe(`Layout Smoke`, () => {
   `, () => {
     cy.visit(`/host-a/`)
 
+    // Check sidebar links exist
+
     cy
     .getByData(`sidebar`)
     .should(`exist`)
@@ -26,6 +28,8 @@ describe(`Layout Smoke`, () => {
     .getByData(`nav-link-reports`)
     .should(`exist`)
 
+    // Check navigation between pages
+    
     cy
     .getByData(`nav-link-about`)
     .click()
@@ -41,6 +45,8 @@ describe(`Layout Smoke`, () => {
     cy
     .getByData(`home-page`)
     .should(`exist`)
+
+    // TODO: add env to implement transition between applications
 
     // cy
     // .getByData(`nav-link-dashboard`)
