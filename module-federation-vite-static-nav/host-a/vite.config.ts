7caa9dba71756043a85c9a7765d7d274c2471c19
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2026-06-29 14:34:06 +0500

    feat: #504: add env to module-federation-vite-static-nav project

--- module-federation-vite-static-nav/host-a/vite.config.ts
+++ module-federation-vite-static-nav/host-a/vite.config.ts
@@ -1,49 +1,40 @@
 /* eslint-disable @typescript-eslint/quotes */
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { federation } from '@module-federation/vite'
 
-const HOST_PORT = process.env.NODE_ENV === 'production'
-  ? 30091
-  : 4001
-
-const LAYOUT_PORT = 4000
-
-const LAYOUT_URL = process.env.NODE_ENV === 'production'
-  ? '/layout/remoteEntry.js'
-  : `http://localhost:${LAYOUT_PORT}/layout/remoteEntry.js`
-
-const ORIGIN = process.env.NODE_ENV === 'production'
-  ? '/'
-  : `http://localhost:${HOST_PORT}`
-
 // eslint-disable-next-line import/no-default-export
-export default defineConfig({
-  server: {
-    port: HOST_PORT,
-    origin: ORIGIN,
-  },
-  base: '/host-a',
-  plugins: [
-    react(),
-    federation({
-      name: 'host_a',
-      remotes: {
-        layout: {
-          type: 'module',
-          name: 'layout',
-          entry: LAYOUT_URL,
+export default defineConfig(({
+  mode,
+}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      port: Number(env.HOST_PORT),
+      origin: env.ORIGIN,
+    },
+    base: '/host-a',
+    plugins: [
+      react(),
+      federation({
+        name: 'host_a',
+        remotes: {
+          layout: {
+            type: 'module',
+            name: 'layout',
+            entry: env.LAYOUT_URL,
+          },
         },
-      },
-      filename: 'remoteEntry.js',
-      shared: {
-        react: {
-          singleton: true,
+        shared: {
+          react: {
+            singleton: true,
+          },
         },
-      },
-    }),
-  ],
-  build: {
-    target: 'esnext',
-  },
+      }),
+    ],
+    build: {
+      target: 'esnext',
+    },
+  }
 })
